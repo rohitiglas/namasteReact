8a16f0ab24ae796482b0254145efcd6bf835fe95
rohit_bansal1 <rohit_bansal1@epam.com>
2023-09-16 12:46:44 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React"
+);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 Tag"),
+    React.createElement("h2", {}, "I am H2 Tag"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
